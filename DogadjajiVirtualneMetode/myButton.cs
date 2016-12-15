833f9c14c1a9db7a84755224adfa52e0d52aa83a
Ivan Bernatović <ivbernat@vsite.hr>
2016-12-15 20:52:08 +0100

    djelomično myButton

---
+++ DogadjajiVirtualneMetode/myButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vsite.CSharp
+{
+    class myButton : System.Windows.Forms.Button
+    {
+        protected override void OnClick(EventArgs e)
+        {
+            MessageBox.Show("Kliknuo si me!", "Moj botun");
+          //  base.OnClick(e);
+        }
+    }
+}
